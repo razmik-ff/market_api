3903c7079a76ff388527e9edc7113a2459b2e863
Razmik Poghosyan <rpoghosyan@fireflies.am>
2023-07-01 18:16:24 +0400

    Finailized companies app

--- companies/urls.py
+++ companies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import CompanyAPIView
+from .views import CompanyAPIView, CompanyDetailAPIView
 
 
 urlpatterns = [
     path('', CompanyAPIView.as_view()),
+    path('<uuid:c_pk>', CompanyDetailAPIView.as_view()),
 ]
--- companies/views.py
+++ companies/views.py
@@ -1,9 +1,10 @@
 from rest_framework import views, status
 from rest_framework.response import Response
+
 from .models import Company
 from .serializers import CompanyGetSerializer
 
-# Create your views here.
+
 class CompanyAPIView(views.APIView):
 
     serializer_class = CompanyGetSerializer
@@ -19,3 +20,25 @@ class CompanyAPIView(views.APIView):
         company = serializer.save()
         resp = self.serializer_class(company)
         return Response(resp.data, status=status.HTTP_201_CREATED)
+
+
+class CompanyDetailAPIView(views.APIView):
+
+    serializer_class = CompanyGetSerializer
+
+    def get(self, request, c_pk):
+        queryset = Company.objects.get(pk=c_pk)
+        companies = CompanyGetSerializer(queryset)
+        return Response(companies.data)
+
+    def patch(self, request, c_pk):
+        company = Company.objects.filter(pk=c_pk).first()
+        serializer = CompanyGetSerializer(company, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_206_PARTIAL_CONTENT)
+
+    def delete(self, request, c_pk):
+        company = Company.objects.filter(pk=c_pk)
+        company.delete()
+        return Response({"msg": "Successfully deleted"}, status=status.HTTP_204_NO_CONTENT)
